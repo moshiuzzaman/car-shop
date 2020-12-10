da494cc90a548753c6ee8da40c3ae0f4efaa41d7
moshiuzzaman <md.shozon041@gmail.com>
2020-12-11 02:26:17 +0600

    add car , update car, and delete car

--- src/Component/CarDetails/CarDetails.js
+++ src/Component/CarDetails/CarDetails.js
@@ -1,25 +1,49 @@
-import React from 'react';
-import { Col, Container, Row } from 'react-bootstrap';
-import { useParams } from 'react-router-dom';
-import { carData } from '../../CarData';
+import React, { useEffect, useState } from 'react';
+import { Button, Col, Container, Row } from 'react-bootstrap';
+import { Link, useHistory, useParams } from 'react-router-dom';
 import Header from '../Header/Header';
 import './CarDetails.css'
 const CarDetails = () => {
-    const {id}=useParams()
-    const carDetails =carData.find(cd=> cd.id==id)
-    const { name, image, price }=carDetails
+    const history=useHistory()
+    const [allCars, setAllCars] = useState([])
+    useEffect(() => {
+        fetch('http://localhost:5000/allCars')
+            .then(response => response.json())
+            .then(data => setAllCars(data))
+    }, [])
+    const { id } = useParams()
+    const carDetails = allCars.find(cd => cd._id === id)
+    const deleteHandler = (id) => {
+        fetch(`http://localhost:5000/carDelete/${id}`, {
+            method: 'DELETE',
+        })
+            .then(response => response.json())
+            .then(data => {
+                if (data) {
+                    alert('service Delete successfully')
+                    history.push('/')
+            }
+        })}
     return (
         <Container>
-           <Header/>
-           <Row>
-               <Col md={6}>
-                   <img className="w-100" src={image} alt=""/>
-               </Col>
-               <Col md={6}>
-                   <h2>{name}</h2>
-                   <h5>${price}</h5> 
-               </Col>
-           </Row>
+            <Header />
+            {
+                carDetails === undefined ? <p>lodding....</p> :
+                    <Row>
+                        <Col md={6}>
+                            <img className="car-image w-100" src={`data:image/png;base64,${carDetails.image.img}`} alt="" />
+                        </Col>
+                        <Col className="d-flex align-items-center" md={6}>
+                            <div>
+                                <h2>{carDetails.carName}</h2>
+                                <h5> Price : ${carDetails.price}</h5>
+                                <p>{carDetails.carDiscription}</p>
+                                <Link to={`/editDetails/${id}`}><Button variant="primary">Edit Details</Button></Link>
+                                <Button onClick={() =>deleteHandler(id)}>Delete</Button>
+                            </div>
+                        </Col>
+                    </Row>
+            }
         </Container>
     );
 };
---
+++ src/Component/CarDetails/EditDetails.js
@@ -0,0 +1,65 @@
+import React, { useEffect, useState } from 'react';
+import { Button, Col, Container, Form } from 'react-bootstrap';
+import { useForm } from 'react-hook-form';
+import { useParams } from 'react-router-dom';
+import Header from '../Header/Header';
+
+const EditDetails = () => {
+    const [allCars, setAllCars] = useState([])
+    useEffect(() => {
+        fetch('http://localhost:5000/allCars')
+            .then(response => response.json())
+            .then(data => setAllCars(data))
+    }, [])
+    const { id } = useParams()
+    const carDetails = allCars.find(cd => cd._id === id)
+    const { register, handleSubmit } = useForm();
+    const onSubmit = (data, e) => {
+        console.log(data);
+        const formData = new FormData()
+        formData.append('file', data.image[0])
+        formData.append('carName', data.carName)
+        formData.append('price', data.price)
+        formData.append('carDiscription', data.carDiscription)
+        formData.append('brandName', data.brandName)
+        fetch(`http://localhost:5000/editDetails/${id}`, {
+            method: 'PATCH',
+            body: formData
+        })
+            .then(response => response.json())
+            .then(data => {
+                if (data) {
+                    alert('Successfully Updated cars')
+                    e.target.reset();
+                }
+            })
+    }
+    return (
+        <Container>
+            <Header />
+            {
+                carDetails !== undefined && <div>
+                    <Form onSubmit={handleSubmit(onSubmit)}>
+                        <Form.Row className='dashboard-admin-form'>
+                            <Form.Group as={Col} md='6' controlId="formGridName">
+                                <Form.Label>Car name</Form.Label>
+                                <Form.Control type="text" defaultValue={carDetails.carName} name='carName' placeholder="Car Name" ref={register({ required: true })} />
+                            </Form.Group>
+                            <Form.Group as={Col} md='6' controlId="formGridPrice">
+                                <Form.Label>Price</Form.Label>
+                                <Form.Control type="number" name='price' defaultValue={carDetails.price} placeholder="Price" ref={register({ required: true })} />
+                            </Form.Group>
+                            <Form.Group as={Col} md='12' controlId="formGridDetails">
+                                <Form.Label>Car Details</Form.Label>
+                                <Form.Control name='carDiscription' defaultValue={carDetails.carDiscription} placeholder="Enter Car Details" as="textarea" rows="3" ref={register({ required: true })} />
+                            </Form.Group>
+                        </Form.Row>
+                        <Button className="Button-add-car-form mb-5" type="submit">UpdateDetails</Button>
+                    </Form>
+                </div>
+            }
+        </Container>
+    );
+};
+
+export default EditDetails;
